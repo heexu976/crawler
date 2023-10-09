6a13d729a912b9ce723f1a1a665576ee7b5ebe84
heexu976 <heexu976@gmail.com>
2023-10-10 01:18:56 +0800

    add collection

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/heexu976/crawler
 
 go 1.19
+
+require (
+	golang.org/x/net v0.16.0
+	golang.org/x/text v0.13.0
+)
--- main.go
+++ main.go
@@ -2,26 +2,16 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
+	"github.com/heexu976/crawler/collect"
 )
 
 func main() {
-	url := "https://www.baidu.com"
-	resp, err := http.Get(url)
+	url := "https://studygolang.com/"
+	f := collect.BrowserFetch{}
+	body, err := f.Get(url)
 	if err != nil {
-		fmt.Printf("fetch url error %v", err)
+		fmt.Println("ERROR")
 		return
 	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error status code : %v", resp.StatusCode)
-		return
-	}
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf("read content failed : %v", err)
-		return
-	}
-	fmt.Println("body: ", string(body))
+	fmt.Println(string(body))
 }
